41dda02072d7aa41417d4b20105986b26d4f6928
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-03-21 16:45:10 -0500

    finished

--- app/app.js
+++ app/app.js
@@ -1,10 +1,34 @@
-angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider, $urlRouterProvider ) {
-  $stateProvider
-    .state('home',{
-      url:'/',
-      templateUrl: "app/about/aboutTmpl.html"
-    });
+angular
+  .module("devmtnTravel", ["ui.router"])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("home", { url: "/", templateUrl: "app/about/aboutTmpl.html" })
+      .state("packages", {
+        url: "/packages/:country",
+        templateUrl: "app/packages/packagesTmpl.html",
+        controller: "packagesCtrl"
+      })
+      .state("locations", {
+        url: "/locations",
+        templateUrl: "app/locations/locationsTmpl.html",
+        controller: "locationsCtrl"
+      })
+      .state("booked", {
+        url: "/booked/:id",
+        templateUrl: "app/booked/bookedTmpl.html",
+        controller: "bookedCtrl"
+      })
+      .state("adventurers", {
+        url: "/adventurers",
+        templateUrl: "app/about/adventurers/adventurers.Tmpl.html",
+        parent: 'home'
+      })
+      .state("contact", {
+        url: "/contact",
+        templateUrl: "app/contact/contactTmpl.html",
+        parent: 'home'
+        
+      });
 
-  $urlRouterProvider
-    .otherwise('/');
-});
+    $urlRouterProvider.otherwise("/");
+  });
